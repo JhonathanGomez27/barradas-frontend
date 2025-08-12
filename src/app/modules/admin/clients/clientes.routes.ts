83d456a32dcb1b877c8413282530311ad9ee746b
JhonathanGomez27 <jhonathango2741@gmail.com>
2025-08-12 17:38:20 -0500

    feat(clients): update clients module configuration and remove obsolete tests

---
+++ src/app/modules/admin/clients/clientes.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ClientsComponent } from './clients.component';
+
+export default [
+    {
+        path: '',
+        component: ClientsComponent
+    },
+] as Routes;
