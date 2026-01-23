3c3c6f9352ddf2b815c1cba1e27349f7923dc51e
JhonathanGomez27 <jhonathango2741@gmail.com>
2026-01-23 16:59:53 -0500

    refactor(web-rtc): comment out socketUrl and related connection logic for future implementation

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,12 +3,12 @@ const hostUrl = 'http://localhost:4200';
 // const socketUrl = 'http://localhost:3000/web-rtc';
 // const host = 'https://www.confiabarradas.com';
 // const hostUrl = 'https://app.confiabarradas.com';
-const socketUrl = 'https://www.confiabarradas.com/web-rtc';
+// const socketUrl = 'https://www.confiabarradas.com/web-rtc';
 
 export const environment = {
     host: `${host}`,
     url: `${host}/api`,
-    socketUrl: `${socketUrl}`,
+    // socketUrl: `${socketUrl}`,
     hostComplete: `${hostUrl}/complete`,
     pagination: 20,
     production: false,
