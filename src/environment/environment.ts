3c212bf1444d35175e1de2f5d8cb99ad2fb101cd
JhonathanGomez27 <jhonathango2741@gmail.com>
2026-02-07 18:12:47 -0500

    fix(environment): revert production server IP to localhost for development
    - Updated environment configuration to switch back from production server IP addresses to localhost for host and hostUrl, facilitating local development.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,8 +1,8 @@
-// const host = 'http://localhost:3000';
-// const hostUrl = 'http://localhost:4200';
+const host = 'http://localhost:3000';
+const hostUrl = 'http://localhost:4200';
 // const socketUrl = 'http://localhost:3000/web-rtc';
-const host = 'http://3.150.32.225';
-const hostUrl = 'http://3.150.32.225';
+// const host = 'http://3.150.32.225';
+// const hostUrl = 'http://3.150.32.225';
 // const socketUrl = 'https://www.confiabarradas.com/web-rtc';
 
 export const environment = {
